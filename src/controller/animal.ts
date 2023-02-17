ac8bd623fc5ed96b24f0861a513ea5f4d0710e3c
cafesao <my@cafesao.net>
2023-02-17 17:02:13 -0300

    feat: add controller animal

--- src/controller/animal.ts
+++ src/controller/animal.ts
@@ -1,10 +1,134 @@
+import { Client, Andress, Animal, Problem } from "@prisma/client"
 import { Request, Response } from "express"
 
+import connectDB from "../db/connect"
+
 const controller = {
-  create: (req: Request, res: Response) => {},
-  read: (req: Request, res: Response) => {},
-  update: (req: Request, res: Response) => {},
-  delete: (req: Request, res: Response) => {}
+  Create: async (req: Request, res: Response) => {
+    const body = req.body
+    try {
+      const db = await connectDB()
+      const response = await db.animal.create({
+        data: body
+      })
+      res.sendStatus(201)
+    } catch (error: any) {
+      if (error.code === "P2003") {
+        return res.status(404).json({ error: "Not found clientId" })
+      }
+      if (error.code === "P2011") {
+        return res.status(400).json({ error: "Required clientId" })
+      }
+      console.log(error)
+      return res.sendStatus(500)
+    }
+  },
+  Read: async (req: Request, res: Response) => {
+    const id = Number(req.query.id)
+
+    try {
+      let response:
+        | (Animal & {
+            problem: Problem | null
+            client: (Client & { andress: Andress | null }) | null
+          })
+        | (Client & {
+            andress: Andress | null
+            animal: (Animal & { problem: Problem | null })[]
+          })
+        | null = null
+      const db = await connectDB()
+      if (id) {
+        response = await db.animal.findFirst({
+          where: {
+            id
+          },
+          include: {
+            client: {
+              include: {
+                andress: true
+              }
+            },
+            problem: true
+          }
+        })
+      }
+
+      if (response === null) {
+        return res.sendStatus(404)
+      }
+      return res.json(response).send()
+    } catch (error: any) {
+      console.log(error)
+      return res.sendStatus(500)
+    }
+  },
+  Update: async (req: Request, res: Response) => {
+    const body = req.body
+    const id = Number(req.query.id)
+
+    try {
+      let response:
+        | (Animal & {
+            problem: Problem | null
+            client: (Client & { andress: Andress | null }) | null
+          })
+        | (Client & {
+            andress: Andress | null
+            animal: (Animal & { problem: Problem | null })[]
+          })
+        | null = null
+      const db = await connectDB()
+      if (id) {
+        response = await db.client.update({
+          data: body,
+          where: {
+            id
+          },
+          include: {
+            andress: true,
+            animal: {
+              include: {
+                problem: true
+              }
+            }
+          }
+        })
+      }
+
+      return res.json(response).send()
+    } catch (error: any) {
+      if (error.code === "P2025") {
+        return res.sendStatus(404)
+      } else {
+        console.log(error)
+        return res.sendStatus(500)
+      }
+    }
+  },
+  Delete: async (req: Request, res: Response) => {
+    const id = Number(req.query.id)
+
+    try {
+      const db = await connectDB()
+      if (id) {
+        await db.client.delete({
+          where: {
+            id
+          }
+        })
+      }
+
+      return res.sendStatus(200)
+    } catch (error: any) {
+      if (error.code === "P2025") {
+        return res.sendStatus(404)
+      } else {
+        console.log(error)
+        return res.sendStatus(500)
+      }
+    }
+  }
 }
 
 export default controller
--- src/routes/animal.ts
+++ src/routes/animal.ts
@@ -3,9 +3,9 @@ import controller from "../controller/animal"
 
 const routes = Router()
 
-routes.post("/", controller.create)
-routes.get("/:typeDoc/:numDoc", controller.read)
-routes.patch("/:typeDoc/:numDoc", controller.update)
-routes.delete("/:typeDoc/:numDoc", controller.delete)
+routes.post("/", controller.Create)
+routes.get("/", controller.Read)
+routes.patch("/", controller.Update)
+routes.delete("/", controller.Delete)
 
 export default routes
